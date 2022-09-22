2c0a9ea0cff37b3fde37e02a6bcb4714285dd2e1
LaToya Simon <1stforgottentoy@gmail.com>
2022-09-21 23:12:26 -0700

    adding cors to app.js

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ const errorHandler = require("./errors/errorHandler");
 
 const notFound = require("./errors/notFound");
 
+const cors = require("cors");
 const app = express();
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
